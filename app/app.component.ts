0431210b9d80a559ebd9e6f544de4b80a99a2e1a
Matthieu <mat@osboxes>
2016-05-12 19:42:23 +0100

    tutorial step2

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><br>My First ts hello world'
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
+import {MatComponent} from './mat.component';
 
 bootstrap(AppComponent);
+bootstrap(MatComponent);
