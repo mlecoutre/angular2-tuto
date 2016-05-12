e8529c19e1c58933b0763d80cc999ffdc7066fe3
mlecoutre <mlecoutre@gmail.com>
2016-05-12 20:55:43 +0100

    tutorial step3

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,8 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {MatComponent} from './mat.component';
+import {HeroDetailComponent} from './hero-detail.component';
 
 bootstrap(AppComponent);
+bootstrap(HeroDetailComponent);
 bootstrap(MatComponent);
