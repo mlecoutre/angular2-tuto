1cc051dcb1b23516b0996daf2b63ec7b2b095864
mlecoutre <mlecoutre@gmail.com>
2016-05-13 17:08:34 +0100

    tutorial step5

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,50 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><br>My First ts hello world'
+  selector: 'my-app',
+  template: `
+      <h1>{{title}}</h1>
+      <nav>
+  <a [routerLink]="['Heroes']">Heroes</a>
+  <a [routerLink]="['Dashboard']">Dashboard</a>
+  <router-outlet></router-outlet>
+  </nav>
+  `,
+  styleUrls: ['app/app.component.css'],
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+  		ROUTER_PROVIDERS,
+		HeroService
+	]
 })
-export class AppComponent { }
+
+@RouteConfig([
+	{
+  	path: '/detail/:id',
+  	name: 'HeroDetail',
+  	component: HeroDetailComponent
+	},
+	{
+  	path: '/dashboard',
+  	name: 'Dashboard',
+  	component: DashboardComponent,
+  	useAsDefault: true
+	},
+	{
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  	}
+])
+
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,4 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
-import {MatComponent} from './mat.component';
-import {HeroDetailComponent} from './hero-detail.component';
 
 bootstrap(AppComponent);
-bootstrap(HeroDetailComponent);
-bootstrap(MatComponent);
